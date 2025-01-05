34a67cb03621657eb65c81b31a350657ab0c8b3a
Joon Park <jsp220@gmail.com>
2025-01-04 21:39:07 -0800

    Set up main page

---
+++ src/components/index.tsx
@@ -0,0 +1,15 @@
+
+const MainPage = () => {
+    return (
+        <>
+            <div>Header</div>
+            <main>
+                <section>
+                    section
+                </section>
+            </main>
+        </>
+    )
+}
+
+export default MainPage;
