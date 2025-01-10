3d4c049faf8e22fbae594a0a38e8bc1d4f892540
Joon Park <jsp220@gmail.com>
2025-01-10 12:02:13 -0800

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
-import './App.css'
-import MainPage from './components'
+import { Route, Routes } from 'react-router-dom'
+import './App.scss'
+import Home from './components/pages/Home'
+import Footer from './components/Footer'
+import Header from './components/Header'
+import About from './components/pages/About'
 
-const App = () => <MainPage />
+const App = () => {
+    return (
+        <>
+            <Header />
+            <main>
+                <section className="container-fluid main-section">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                    </Routes>
+                </section>
+            </main>
+            <Footer />
+        </>
+    )
+}
 
-export default App
+export default App;
---
+++ src/components/Header.tsx
@@ -0,0 +1,66 @@
+import { Link, useLocation } from 'react-router-dom';
+
+const Header = () => {
+    const location = useLocation();
+    const currentPage = location.pathname === "/" ? "home" : location.pathname.slice(1);
+    
+    return (
+        <header>
+            <nav className="navbar navbar-expand-lg justify-content-between">
+                <div className="container-fluid">
+                    <Link
+                        to="/"
+                        className="col-4 ms-2 fw-bolder navbar-brand full-name"
+                    >
+                        Joon Park
+                    </Link> 
+                    <ul className="col-7 nav justify-content-end ms-auto mt-2 mt-lg-0 text-end">
+                        <li className="nav-item">
+                            <Link 
+                                to="/"
+                                className={currentPage === 'home' ? 'nav-link fw-bolder' : 'nav-link'}
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link 
+                                to="/about"
+                                className={currentPage === 'about' ? 'nav-link fw-bolder' : 'nav-link'}
+                            >
+                                About
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link 
+                                to="/portfolio"
+                                className={currentPage === 'portfolio' ? 'nav-link fw-bolder' : 'nav-link'}
+                            >
+                                Portfolio
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link 
+                                to="/contact"
+                                className={currentPage === 'contact' ? 'nav-link fw-bolder' : 'nav-link'}
+                            >
+                                Contact
+                            </Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link
+                                to="/resume"
+                                className={currentPage === 'resume' ? 'nav-link fw-bolder' : 'nav-link'}
+                            >
+                                Resume
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <h1 className="headline">Full Stack Web Developer</h1>
+        </header>
+    )
+}
+
+export default Header;
